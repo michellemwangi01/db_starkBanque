c729784e5665386d0dc3d545809337be68edc517
michellemwangi01 <michellemwangim@gmail.com>
2023-07-09 23:39:02 +0300

    initial db commit

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
-// const jsonServer = require("json-server")
-// const server = jsonServer.create();
-// const router = jsonServer("db.json")
-// const middlewares = jsonServer.defaults()
-// const port = process.env.PORT || 8080
-
-// server.use(middlewares)
-// server.use(router)
-// server.listen(port)
-
-
-const jsonServer = require("json-server");
-const cors = require("cors");
-const path = require("path");
+const jsonServer = require("json-server")
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db", "db.json"));
-const middlewares = jsonServer.defaults();
+const router = jsonServer.router("db.json")
+const middlewares = jsonServer.defaults()
+const port = process.env.PORT || 3001
 
-server.use(cors());
-server.use(jsonServer.bodyParser);
-server.use(middlewares);
-server.use(router);
+server.use(middlewares)
+server.use(router)
+server.listen(port)
 
-const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`);
-});
+
